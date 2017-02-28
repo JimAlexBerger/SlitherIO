6fc100ecd2f953698140842f02368d466fcc9504
Jim-Alexander Berger Gundersen <alexandergundersen@yahoo.no>
2017-02-28 02:26:52 +0100

    TODO

---
+++ src/snake/Snake.java
@@ -0,0 +1,15 @@
+package snake;
+
+public class Snake {
+	
+	//TO DO VARIEBLES
+	
+	//neural net
+	
+	//TO DO METHODS
+	
+	//GETINPUTS(rgb values at screen put into array)
+	
+	//FROM angle to mouse pos(use cos and sin)
+
+}
